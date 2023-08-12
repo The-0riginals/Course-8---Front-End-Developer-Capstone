97e136aa90e206f3fde3f64d0750ebd74e544404
Klaus <whynotmemylove99@gmail.com>
2023-08-13 01:44:12 +0700

    add hero for home

--- little-lemon-capstone-project/src/App.js
+++ little-lemon-capstone-project/src/App.js
@@ -6,17 +6,20 @@ import Contact from "./pages/Contact/Contact";
 import pages from "./utils/pages";
 import Reserve from "./pages/Reserve/Reserve";
 import Menu from "./pages/Menu/Menu";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div data-testid="app-component">
       <Layout>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+
           <Route path={pages.get("about").path} element={<About />} />
           <Route path={pages.get("contact").path} element={<Contact />} />
           <Route path={pages.get("reserve").path} element={<Reserve/>} />
           <Route path={pages.get("menu").path} element={<Menu/>} />
+          <Route path={pages.get("home").path} element={<Home/>} />
+          <Route path="/" element={<h1>Home</h1>} />
         </Routes>
       </Layout>
     </div>
